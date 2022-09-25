5235580c7fa77aa8c2d5b125396cc296dad362b6
Baran Rolando <Rolphoe@gmail.com>
2022-09-25 10:53:12 +0300

    ABC_1 - Project setup

---
+++ src/main/java/org/example/PractigalProjectGitDemoMain.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class PractigalProjectGitDemoMain {
+    public static void main(String[] args) {
+
+        System.out.println("Project starting!");
+        System.out.println("Project starting!!");
+    }
+}
